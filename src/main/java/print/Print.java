ebb915b24e40575d7a1de7bf7ff8c4d6f2f723b3
PersesTitan <torampersestitan@gmail.com>
2022-04-04 13:11:23 +0900

    commit

--- src/main/java/print/Print.java
+++ src/main/java/print/Print.java
@@ -3,13 +3,14 @@ package print;
 import jakarta.validation.constraints.NotNull;
 
 public class Print {
-    private String start;
+    String start;
     private String end;
 
     public Print(@NotNull String start, @NotNull String end) {
         this.start = start;
         this.end = end;
     }
+
 
 
 }
