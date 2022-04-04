934ee50a2e81726acf6e4d01c7cefb6506789390
PersesTitan <torampersestitan@gmail.com>
2022-04-04 13:11:23 +0900

    commit

--- src/main/java/print/Print.java
+++ src/main/java/print/Print.java
@@ -6,7 +6,7 @@ public class Print {
     private String start = null;
     private String end = null;
 
-    public Print(@NotNull String start) {
+    public Print(@NotNull String start, String end) {
 
     }
 }
