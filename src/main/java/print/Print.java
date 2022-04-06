41b5f10233778c1d9da8f13cb7afd278c71b8dc7
PersesTitan <torampersestitan@gmail.com>
2022-04-06 13:11:23 +0900

    commit

--- src/main/java/print/Print.java
+++ src/main/java/print/Print.java
@@ -2,17 +2,27 @@ package print;
 
 import jakarta.validation.constraints.NotNull;
 
+import java.io.IOException;
+
 public class Print {
     String start = null;
     String end = null;
 
-    public Print(@NotNull String start, @NotNull String end) {
+    public Print(@NotNull String start, @NotNull String end, @NotNull boolean mode) {
         this.start = start;
         this.end = end;
     }
 
-    public Print(@NotNull String var, @NotNull Local local) {
-
+    /**
+     * @param var
+     * @param local
+     * @throws IOException
+     */
+    public Print(@NotNull String var, @NotNull Local local) throws IOException {
+        if (local.equals(Local.START)) this.start = var;
+        else if (local.equals(Local.END)) this.end = var;
+        else throw new IOException("Local 값 오류");
     }
+
 
 }
